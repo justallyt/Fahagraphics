44e758f4322966373f54b8e43afe40553ccc0bc2
justallyt <albertjunk2030@gmail.com>
2023-11-29 18:31:19 +0300

    updates

--- src/components/ProductsBody.jsx
+++ src/components/ProductsBody.jsx
@@ -1,7 +1,30 @@
-
+import { GoSearch } from "react-icons/go";
+import { products } from "../data/products";
 const ProductsBody = () => {
   return (
-    <div>ProductsBody</div>
+    <div className="products-body">
+                <div className="products-body-content">
+                             <div className="products-body-header">
+                                          <h2>All Products</h2>
+                                          <div className="search-bar-area">
+                                                    <span><GoSearch /></span>
+                                                    <input type="text" className="search-control" placeholder="Search products here" />
+                                          </div>
+                             </div>
+
+                             <div className="products-row">
+                                       { products.map(item => 
+                                             <div className="product-moja" key={item.id}>
+                                                       <div className="product-image">
+                                                                <img src={item.main_image} alt="" />
+                                                       </div>
+                                                        <h3>{item.title}</h3>
+                                             </div>
+                                        
+                                        )}
+                             </div>
+                </div>
+    </div>
   )
 }
 
---
+++ src/data/products.js
@@ -0,0 +1,84 @@
+export const products = [
+     {
+          id: 0,
+          title: 'Solvent Printing Inks',
+          main_cateogry: 'Printing Inks',
+          product_description: '',
+          subcategory: '',
+          link: '/product/printing-inks/solvent-printing-inks',
+          main_image: '/products/printingInks/solventPrintingInks/image2.jpg',
+          product_images: ['/products/printingInks/solventPrintingInks/image3.jpg', '/products/printingInks/solventPrintingInks/image1.jpg'],
+     },
+     {
+        id: 1,
+        title: 'Varnishes',
+        main_cateogry: 'Printing Inks',
+        product_description: '',
+        subcategory: '',
+        link: '/product/printing-inks/varnishes',
+        main_image: '/products/printingInks/varnishes/image1.jpg',
+        product_images: ['/products/printingInks/varnishes/image2.jpg', '/products/printingInks/varnishes/image3.jpg', '/products/printingInks/varnishes/image5.jpg', '/products/printingInks/varnishes/image4.jpg'],
+   },
+   {
+    id: 2,
+    title: 'Vehicles Spray',
+    main_cateogry: 'Printing Inks',
+    product_description: '',
+    subcategory: '',
+    link: '/product/printing-inks/vehiclespray',
+    main_image: '/products/printingInks/vehiclespray/image1.jpg',
+    product_images: [''],
+},
+{
+    id: 3,
+    title: 'UV Offset Inks',
+    main_category: 'Printing Inks',
+    product_description: '',
+    subcategory: 'UV Printing Inks',
+    link: '/product/printing-inks/uv-printing-inks/uv-offset-inks',
+    main_image: '/products/printingInks/uv/offset/image2.jpg',
+    product_images: ['/products/printingInks/uv/offset/image1']
+},
+{
+     id: 4,
+     title: 'UV Flexo Inks',
+     main_category: 'Printing Inks',
+     product_description: '',
+     subcategory: 'UV Printing Inks',
+     link: '/product/printing-inks/uv-printing-inks/uv-flexo-inks',
+     main_image: '/products/printingInks/uv/flexo/image1.jpg',
+     product_images:['/products/printingInks/uv/offset/image2.jpg']
+},
+{
+     id: 5,
+     title: 'Water Based Printing Inks',
+     main_category: 'Printing Inks',
+     product_description: '',
+     subcategory:'Water Based Printing Inks',
+     link: '/product/printing-inks/water-based-printing-inks',
+     main_image: '/products/printingInks/waterbased/image1.jpg',
+     product_images: []
+},
+{
+    id: 6,
+    title: 'Web Offset Printing Inks',
+    main_category: 'Printing Inks',
+    product_description: '',
+    subcategory: 'Offset Printing Inks',
+    minicategory: 'Web Offset Printing Inks',
+    link: '/product/printing-inks/offset-printing-inks/web',
+    main_image: '/products/printingInks/offset/web/image1.jpg',
+    product_images: ['/products/printingInks/offset/web/image2.jpg']
+},
+{
+    id: 7,
+    title: 'FG Lion Series',
+    main_category: 'Printing Inks',
+    product_description: '',
+    subcategory: 'Offset Printing Inks',
+    minicategory: 'Sheetfed Offset Printing Inks',
+    link: '/product/printing-inks/offset-printing-inks/sheetfed',
+    main_image: '/products/printingInks/offset/sheetfed/fglion/image1.jpg',
+    product_images: []
+}
+]
